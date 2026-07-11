284688712422a3da0ec9a372ad94374c61851491
Omjd12 <omjadha9728@gmail.com>
2026-07-11 19:02:12 +0530

    changed the movement logic

--- hope_rebuild.ino
+++ hope_rebuild.ino
@@ -8,7 +8,7 @@ bool server_status = true;
 pid p(40, 0., 1.3);
 motor a(26, 27, 25, 33);
 tcp server("narzo 50A", "123456789");
-unsigned long dt =0;
+unsigned long dt = 0;
 float target = 0.0;
 float angle;
 unsigned long now = 0;
@@ -21,49 +21,53 @@ void setup() {
   a.setPWM(13, 32);  // setting up pwm pins
   Serial.begin(115200);
   server_status = server.connect();
-  if(server_status){
+  if (server_status) {
     server.StartServer();
   }
-  if (m.initialize()) { // initializing mpu
+  if (m.initialize()) {  // initializing mpu
     Serial.println("mpu initialized");
-     if(server_status){
+    if (server_status) {
       server.sendText("mpu initialized");
     }
     iniT = true;
-  }
-  else{
+  } else {
     Serial.println("terminated");
-     if(server_status){
+    if (server_status) {
       server.sendText("terminated");
     }
-    while(1);
+    while (1)
+      ;
   }
   if (iniT) {
-    dmpInit = m.dmpInit(); // initializing dmp 
+    dmpInit = m.dmpInit();  // initializing dmp
   }
   m.caliberate(dt);
-  p.reset(); // refer the control.h library
- // p.setTarget(target);
-  
-  
+  p.reset();  // refer the control.h library
+              // p.setTarget(target);
 }
 
 void loop() {
-  if ( m.check()) {
+  if (m.check()) {
     now = micros();
     dt = now - lastMeasured;
     if (dt >= 10000) {
       m.update();
       angle = m.getYaw();
     } else pass;
-      
-      error = angle - target;
-      output = p.compute(error, dt);
+
+    error = angle - target;
+    output = p.compute(error, dt);
     lastMeasured = now;
   } else pass;
 
-    a.forward(output,error)
-    if(server_status){
-      server.send(angle, now);
-    }
+
+  if (angle < target - 5.0 || angle > target + 5.0) {
+    a.turn(angle, target);
+  } else {
+    a.forward(output, error);
+  }
+  //a.forward(output,error);
+  if (server_status) {
+    server.send(angle, now);
+  }
 }
